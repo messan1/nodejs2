4be128ef58409caf25f081fb1e3663cce2f81e3c
quantum <42383853+messan1@users.noreply.github.com>
2018-11-10 00:05:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,26 +3,18 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
     host: "us-cdbr-iron-east-01.cleardb.net",
     user: "bf0ff675c8b20a",
-    password: "09053071"
+    password: "09053071",
+    database: "heroku_ae0b253dd96f272"
   });
   con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-    con.query("CREATE DATABASE mydb", function (err, result) {
+    var sql = "CREATE TABLE Messan (name VARCHAR(255), address VARCHAR(255))";
+    con.query(sql, function (err, result) {
       if (err) throw err;
-      console.log("Database created");
+      console.log("Table created");
     });
   });
-const port=process.env.PORT || 3000
-const server = http.createServer((req, res) => {
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/html');
-res.end('<h1>Hello World</h1>');
-});
-server.listen(port,() => {
-console.log(`Server running at port `+port);
-});
-
 
 //mysql://bf0ff675c8b20a:09053071@us-cdbr-iron-east-01.cleardb.net/heroku_ae0b253dd96f272?reconnect=true
 
